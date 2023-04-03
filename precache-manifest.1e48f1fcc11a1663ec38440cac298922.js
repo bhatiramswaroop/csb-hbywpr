4296b534d610ddf179755f5731ee20180b467c4b
Code Sandbox <hello@codesandbox.io>
2023-04-03 09:06:23 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1e48f1fcc11a1663ec38440cac298922.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724fbe7db406ea2e3a11026cdafea4ac",
+    "url": "https://.github.io/csb-hbywpr/index.html"
+  },
+  {
+    "revision": "797788c2470c19068180",
+    "url": "https://.github.io/csb-hbywpr/static/css/main.802cedd1.chunk.css"
+  },
+  {
+    "revision": "7326e8f50b5ee0a3224e",
+    "url": "https://.github.io/csb-hbywpr/static/js/2.fa4a670d.chunk.js"
+  },
+  {
+    "revision": "797788c2470c19068180",
+    "url": "https://.github.io/csb-hbywpr/static/js/main.993b4f00.chunk.js"
+  },
+  {
+    "revision": "c25b351b6e31a716e079",
+    "url": "https://.github.io/csb-hbywpr/static/js/runtime~main.f5d5bb6f.js"
+  }
+]);
